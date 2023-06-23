13848d5b0588242f33f0c6e85d0bdeb84b9eea1d
Julien Moulines <julienmoulines@MacBook-Pro-de-Julien.local>
2023-06-23 07:26:46 +0200

    makefile

--- minitalk.h
+++ minitalk.h
@@ -7,13 +7,14 @@
 #include <signal.h>
 #include <string.h>
 #include <pthread.h>
+#include "libft/libft.h"
 
 
 void	count_g_len(int sig, siginfo_t *info, void *context);
-void admin(int sig, siginfo_t *info, void *context);
-int ft_end(siginfo_t *info, unsigned char **str);
+void 	admin(int sig, siginfo_t *info, void *context);
+int 	ft_end(siginfo_t *info, unsigned char **str);
 void	ft_adr(int sig, siginfo_t *sig_info, void *context);
-void ft_len(int pid, int a_len);
-void ft_atob(int pid, char c);
+void 	ft_len(int pid, int a_len);
+void 	ft_atob(int pid, char c);
 
 #endif
